941190be4348e40bca83acfe0a7b8f6ac9aaff37
grmc1999 <grmc1999@gmail.com>
2025-02-20 11:29:06 -0300

    exps

--- bash_routines/run_train.sh
+++ bash_routines/run_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 root='/share_zeta/Proxy-Sim/PhysicsSimulationDeepLearning'
-#cd $root'Encoding_Methods_For_VAE'
+cd $root
 
 python Train.py launch --directory $root'Exps/GAN_PI/GAN_PI_1' --epochs 200
