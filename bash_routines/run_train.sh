e6af579cd71d3d830d28dd497990e5db993294f0
grmc1999 <grmc1999@gmail.com>
2025-03-19 14:27:32 -0300

    merge

--- bash_routines/run_train.sh
+++ bash_routines/run_train.sh
@@ -2,7 +2,8 @@
 
 root='/share_zeta/Proxy-Sim/PhysicsSimulationDeepLearning'
 cd $root
+exp_name='GAN_PI_LL_s10_PDELL_D_os_3_G_os_5'
+echo $exp_name
+#python Train.py launch --directory $root'/Exps/GAN_PI/$exp_name' --epochs 800
 
-#python Train.py launch --directory $root'/Exps/GAN_PI/GAN_PI_w_l3' --epochs 800
-
-python Train.py launch_data_test --directory $root'/Exps/Data_sensibility/GAN_PI/GAN_PI_LL_s10_PDELL' --epochs 500
+python Train.py launch_data_test --directory $root'/Exps/Data_sensibility/GAN_PI/'$exp_name --epochs 500
