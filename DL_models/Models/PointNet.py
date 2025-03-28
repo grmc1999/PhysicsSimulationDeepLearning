ecb64dc67d82266ce5e3f517b4abb966f59073a7
grmc1999 <grmc1999@gmail.com>
2025-03-28 16:01:12 -0300

    PointNet

--- DL_models/Models/PointNet.py
+++ DL_models/Models/PointNet.py
@@ -4,6 +4,7 @@ from torch.autograd import Variable
 import numpy as np
 import torch.nn.functional as F
 import numpy as np
+from einops import rearrange
 
 class STN3d(nn.Module):
     def __init__(self):
@@ -81,10 +82,10 @@ class STNkd(nn.Module):
         return x
 
 class PointNetfeat(nn.Module):
-    def __init__(self, global_feat = True, feature_transform = False):
+    def __init__(self, n_inputs=3, global_feat = True, feature_transform = False):
         super(PointNetfeat, self).__init__()
-        self.stn = STN3d()
-        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.stn = STNkd(k=n_inputs)
+        self.conv1 = torch.nn.Conv1d(n_inputs, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
         self.bn1 = nn.BatchNorm1d(64)
@@ -123,11 +124,11 @@ class PointNetfeat(nn.Module):
             return torch.cat([x, pointfeat], 1), trans, trans_feat
 
 class PointNetDenseCls(nn.Module):
-    def __init__(self, k = 2, feature_transform=False):
+    def __init__(self,n_inputs=3, k = 2, feature_transform=False):
         super(PointNetDenseCls, self).__init__()
         self.k = k
         self.feature_transform=feature_transform
-        self.feat = PointNetfeat(global_feat=False, feature_transform=feature_transform)
+        self.feat = PointNetfeat(n_inputs=n_inputs,global_feat=False, feature_transform=feature_transform)
         self.conv1 = torch.nn.Conv1d(1088, 512, 1)
         self.conv2 = torch.nn.Conv1d(512, 256, 1)
         self.conv3 = torch.nn.Conv1d(256, 128, 1)
@@ -136,7 +137,7 @@ class PointNetDenseCls(nn.Module):
         self.bn2 = nn.BatchNorm1d(256)
         self.bn3 = nn.BatchNorm1d(128)
 
-    def forward(self, x):
+    def forward_o(self, x):
         batchsize = x.size()[0]
         n_pts = x.size()[2]
         x, trans, trans_feat = self.feat(x)
@@ -144,7 +145,13 @@ class PointNetDenseCls(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = self.conv4(x)
-        x = x.transpose(2,1).contiguous()
-        x = F.log_softmax(x.view(-1,self.k), dim=-1)
+        #x = x.transpose(2,1).contiguous()
+        #x = F.log_softmax(x.view(-1,self.k), dim=-1)
         x = x.view(batchsize, n_pts, self.k)
         return x, trans, trans_feat
+    
+    def forward(self,x):
+        #x = x.transpose(2, 1)
+        x=rearrange(x,"b p v -> b v p")
+        x,_,_=self.forward_o(x)
+        return x
