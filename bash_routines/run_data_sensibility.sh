227c1f5e9bf5594862a19affdd8813d0285b1979
grmc1999 <grmc1999@gmail.com>
2025-03-31 17:26:39 -0300

    steps

--- bash_routines/run_data_sensibility.sh
+++ bash_routines/run_data_sensibility.sh
@@ -3,4 +3,4 @@
 root='/share_zeta/Proxy-Sim/PhysicsSimulationDeepLearning'
 cd $root
 
-python Train.py launch_data_test --directory $root'/Exps/Data_sensibility/GAN_PI/GAN_PI_LL_s10_PDELL' --epochs 500
+python Train.py launch_data_test --directory $root'/Exps/Data_sensibility_exps/GAN_PI/GAN_PI_LL_s10_PDELL' --epochs 500
