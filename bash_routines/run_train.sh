4a3cd9856956e8205e390f43061aeb05c7056c0d
grmc1999 <grmc1999@gmail.com>
2025-05-12 18:30:01 -0300

    bash

--- bash_routines/run_train.sh
+++ bash_routines/run_train.sh
@@ -2,9 +2,9 @@
 
 root='/share_zeta/Proxy-Sim/PhysicsSimulationDeepLearning'
 cd $root
-exp_name='GAN_PI__MultiP20_PointNet_LL_PDELL'
+exp_name='GAN_PI_MultiP5_PointNet_LL_PDELL_D2_G2_opt_LBFGS'
 echo $exp_name
-#python Train.py launch --directory $root'/Exps/GAN_PI/'$exp_name --epochs 800
+python Train.py launch --directory $root'/Exps/GAN_PI/'$exp_name --epochs 200
 
 
-python Train.py launch_data_test --directory $root'/Exps/Data_sensibility_exps/GAN_PI/'$exp_name --epochs 500
+#python Train.py launch_data_test --directory $root'/Exps/Data_sensibility_exps/GAN_PI/'$exp_name --epochs 500
