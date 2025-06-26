3d4a3355ada2a58ad073fcd67c83c382a5dc2336
grmc1999 <grmc1999@gmail.com>
2025-06-26 18:35:26 -0300

    rt

--- bash_routines/run_train.sh
+++ bash_routines/run_train.sh
@@ -11,8 +11,9 @@ exp_name='GAN_PI_LL_s10_PDELL_D_os_3_G_os_5'
 data_name='GAN_PI'
 #data_name='2D_Poisson/2D_poisson_eq_D_N_R_quad_f_Linear_Linear_64x64'
 #data_name='2D_Poisson/2D_poisson_eq_Dirichlet_Const_Quad_f_Const_64x64'
-data_name='3D_Poisson/3D_poisson_eq_Dirichlet_BC_64x64x64'
+#data_name='3D_Poisson/3D_poisson_eq_Dirichlet_BC_64x64x64'
 #data_name='2D_NS/2D_NS_eq_Dirichlet_BC_S10x10_T10s'
+data_name='3D_NS/3D_poisson_eq_Dirichlet_BC_64x64x64'
 #data_name='2D_2P_Darcy_flow/2D_NS_eq_Dirichlet_BC_S10x10_T10s'
 
 
