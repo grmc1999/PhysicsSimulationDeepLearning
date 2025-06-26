26ebe22df56fc0a6e215eaf608e20e0eb38a018a
grmc1999 <grmc1999@gmail.com>
2025-06-26 17:32:40 -0300

    SD

--- bash_routines/run_train.sh
+++ bash_routines/run_train.sh
@@ -12,11 +12,12 @@ data_name='GAN_PI'
 #data_name='2D_Poisson/2D_poisson_eq_D_N_R_quad_f_Linear_Linear_64x64'
 #data_name='2D_Poisson/2D_poisson_eq_Dirichlet_Const_Quad_f_Const_64x64'
 #data_name='3D_Poisson/3D_poisson_eq_Dirichlet_BC_64x64x64'
-data_name='2D_NS/2D_NS_eq_Dirichlet_BC_S10x10_T10s'
+#data_name='2D_NS/2D_NS_eq_Dirichlet_BC_S10x10_T10s'
+data_name='2D_2P_Darcy_flow/2D_NS_eq_Dirichlet_BC_S10x10_T10s'
 
 echo $data_name
 echo $exp_name
-python Train.py launch --directory $root'/Exps/'$data_name'/'$exp_name --epochs 300
+python3 Train.py launch --directory $root'/Exps/'$data_name'/'$exp_name --epochs 500
 
 
 #python Train.py launch_data_test --directory $root'/Exps/Data_sensibility_exps/GAN_PI/'$exp_name --epochs 500
