8996a46785b278c9658cb5be03b0d116a6788ccd
grmc1999 <grmc1999@gmail.com>
2025-07-28 15:26:22 -0300

    fix 1

--- Physical_models/Differentiable_simulation.py
+++ Physical_models/Differentiable_simulation.py
@@ -394,7 +394,7 @@ class two_phase_flow_RD_decoupled_DT(two_phase_flow_ReactionDiffusion):
 
 class two_phase_flow_RD_TBK(two_phase_flow_RD_decoupled_DT):
     def __init__(self,phi_w,phi_o,dtphi_w_1,dtphi_o_1,dt,por,mu_w,mu_o,Pc_args,K_s,krwo):
-        super().__init__(phi_w,phi_o,dtphi_w_1,dtphi_o_1,dt,por,mu_w,mu_o,K_s=K_s,Pc_args=Pc_args,kr_w=0.3,kr_o=0.3)
+        super().__init__(phi_w,phi_o,dtphi_w_1,dtphi_o_1,dt,por,mu_w,mu_o,K_s=K_s,Pc_args=Pc_args,kr_w=0.3,kr_o=0.3,max_dt=1e6,min_dt=-1e6)
         self.krwo=krwo
 
         self.K_w=lambda K_l,p_c:stack(
