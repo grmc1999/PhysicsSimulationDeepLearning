87b36fc7b0811bf51463b657e718946a28167be8
grmc1999 <grmc1999@gmail.com>
2025-07-29 15:24:32 -0300

    stat

--- DL_models/Models/CNN_models.py
+++ DL_models/Models/CNN_models.py
@@ -41,4 +41,27 @@ class simple_dual_space_cnn_model(torch.nn.Module):
     #print(x.shape)
     #print(x[:,0].shape)
     return x[:,0],x[:,1],dtx1[0],dtx2[0]
-    #return x[:,0],x[:,1],x[:,2],x[:,3]
+
+
+class simple_dual_space_with_time_derivative_cnn_model(torch.nn.Module):
+  def __init__(self):
+    super(simple_dual_space_cnn_model,self).__init__()
+
+    self.conv1=torch.nn.Conv2d(4, 32, (9,9), padding=4)
+    self.conv2=torch.nn.Conv2d(32, 64, (9,9), padding=4)
+    self.conv3=torch.nn.Conv2d(64, 4, (9,9), padding=4)
+    self.act1=torch.nn.ReLU()
+    self.act2=torch.nn.ReLU()
+    self.act3=torch.nn.Identity()
+  def forward(self,x1,x2,dtx1,dtx2):
+    x=torch.concatenate((x1,x2,dtx1,dtx2),axis=1)
+    #x=torch.concatenate((x1,x2,dtx1,dtx2),axis=1)
+    x=self.act1(self.conv1(x))
+    x=self.act2(self.conv2(x))
+    x=self.act3(self.conv3(x))*1
+
+    #x=torch.clamp(x, min=-0.5, max=0.5)
+    #print(dtx1.shape)
+    #print(x.shape)
+    #print(x[:,0].shape)
+    return x[:,0],x[:,1],x[:,2],x[:,3]
